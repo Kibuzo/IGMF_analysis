ee3d93560039cf2bc31dd57450fa823c050cc86a
Stefano Silvestri <silvestri.sfn@gmail.com>
2022-08-03 13:37:53 +0200

    Implemented the constants file, and some minor addition to IO functions

---
+++ Constants/J1943.py
@@ -0,0 +1,8 @@
+'''File containing all the relevant constants to be used
+in scripts related to the HESS J1943+213 analysis
+'''
+
+D = 887.6799277667532 # Distance in MPC
+E0 = 300. #Decorrelation energy in MeV
+N0 = 3.83e-14 # N0 from the fermi-LAT fit
+Cutoff = 2080 #Cutoff energy in GeV
--- IO/read_crpropa.py
+++ IO/read_crpropa.py
@@ -1,7 +1,7 @@
 import numpy
 import os
 
-def unite_small_files(n_files, path, prefix, B_rms, suffix = 'txt'):
+def unite_small_files(n_files, file_path, prefix, B_rms, suffix = '.txt'):
     '''Reads all small files with the same characteristics from a folder,
     Joins them into a single larger file and returns the corresponding
     array.
@@ -12,19 +12,19 @@ def unite_small_files(n_files, path, prefix, B_rms, suffix = 'txt'):
     '''
     large_file=[] #This needs to exist in case first file is empty
     file_name = (f'{pefix}{B_rms}{suffix}')
-    large_file=numpy.loadtxt(os.path.join(path, file_name))
-    length=len(np.unique(a[11]))
+    large_file=numpy.loadtxt(os.path.join(file_path, file_name))
+    length=len(numpy.unique(a[11]))
     for i in range (1,int(n_files-1)):
         file_name=(f'{prefix}{i}{B_rms}{suffix}')
         if os.path.exists(filename):
-            small_file=np.loadtxt(os.path.join(path, file_name))
-            length+=len(np.unique(s[11]))
+            small_file=numpy.loadtxt(os.path.join(file_path, file_name))
+            length+=len(numpy.unique(s[11]))
             if small_file.shape[0]>0:
                 if (large_file.ndim==1):
-                    large_file=large_file[:, np.newaxis]
+                    large_file=large_file[:, numpy.newaxis]
                 if (small_file.ndim==1):
-                    small_file=small_file[:, np.newaxis]
-                large_file=np.concatenate((large_file,small_file),axis=1)
+                    small_file=small_file[:, numpy.newaxis]
+                large_file=numpy.concatenate((large_file,small_file),axis=1)
             else:
                 print (f'{file_name} is empty, skipping...')
         else:
@@ -32,3 +32,7 @@ def unite_small_files(n_files, path, prefix, B_rms, suffix = 'txt'):
     print(f'A total of {length} primary photons generated the cascade that\
          hit the detector')
     return (large_file)
+
+def load_large_file(file_path, prefix, Brms, suffix='.txt'):
+    file_name = (f'{prefix}{Brms}{suffix}')
+    return (numpy.loadtxt(os.path.join(file_path, file_name)))
